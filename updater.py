42ac7c78d002a67dc72f8549168eb2492f545c25
aallrd <antoine.allard@outlook.com>
2018-07-28 20:20:27 +0200

    Better logs, dry run and record type selection

--- updater.py
+++ updater.py
@@ -15,6 +15,10 @@ logger.addHandler(console_handler)
 
 GANDI_API_TOKEN = None
 GANDI_DOMAINS = []
+GANDI_RECORD_TYPES = ['A', 'AAAA', 'CAA', 'CDS', 'CNAME',
+                      'DNAME', 'DS', 'LOC', 'MX', 'NS', 'PTR',
+                      'SPF', 'SRV', 'SSHFP', 'TLSA', 'TXT', 'WKS']
+DRY_RUN = False
 
 
 def get_gandi_api_token():
@@ -37,6 +41,7 @@ def get_domain_records(domain):
         uri = "https://dns.api.gandi.net/api/v5/domains/{}/records".format(domain)
         headers = {'X-Api-Key': GANDI_API_TOKEN}
         r = get(uri, headers=headers)
+        logger.debug("Gandi API response: {}".format(r.json()))
         if not r.status_code == 200:
             raise exceptions.HTTPError("HTTP error: {}: {}".format(r.status_code, r.reason))
         else:
@@ -63,25 +68,37 @@ def get_records_www_ip(records):
 
 
 def update_gandi_domain_records(records, old_ip, new_ip):
+    global GANDI_RECORD_TYPES
     try:
         for record in records:
-            for index, rrset_value in enumerate(record['rrset_values']):
-                if rrset_value == old_ip:
-                    record['rrset_values'][index] = new_ip
+            if record['rrset_type'] in GANDI_RECORD_TYPES:
+                for index, rrset_value in enumerate(record['rrset_values']):
+                    if rrset_value == old_ip:
+                        record['rrset_values'][index] = new_ip
+                    else:
+                        logger.debug("{}: {} ({})".format(record['rrset_type'], rrset_value, index))
+            else:
+                logger.debug("Discarding record type {} not in targeted list {}.".format(record['rrset_type'],
+                                                                                         GANDI_RECORD_TYPES))
         return records
     except Exception as e:
         raise Exception("Failure while updating the IP address for domain records: {}".format(e))
 
 
 def push_updated_domain_records(domain, updated_records):
-    global GANDI_API_TOKEN
+    global GANDI_API_TOKEN, DRY_RUN
     try:
         uri = "https://dns.api.gandi.net/api/v5/domains/{}/records".format(domain)
         headers = {'X-Api-Key': GANDI_API_TOKEN, 'Content-Type': 'application/json'}
         data = {'items': updated_records}
-        r = put(uri, headers=headers, data=json.dumps(data))
-        r.raise_for_status()
-        logger.info("Records updated for domain {}".format(domain))
+        if not DRY_RUN:
+            r = put(uri, headers=headers, data=json.dumps(data))
+            r.raise_for_status()
+            logger.info("Records updated for domain {}".format(domain))
+        else:
+            logger.info("URI: {}".format(uri))
+            logger.info("headers: {}".format(headers))
+            logger.info("data: {}".format(data))
     except exceptions.HTTPError:
         raise exceptions.HTTPError("HTTP error: {}: {}".format(r.status_code, r.json()))
     except exceptions.RequestException as e:
@@ -96,7 +113,10 @@ def get_livebox_wan_ip():
         params = '''{"service":"NMC","method":"getWANStatus","parameters":{}}'''
         headers = {"Content-type": "application/x-sah-ws-4-call+json"}
         r = post("http://livebox/ws", headers=headers, data=params).json()
-        return r["data"]["IPAddress"]
+        logger.debug("Livebox WS response: {}".format(r))
+        livebox_wan_ip = r["data"]["IPAddress"]
+        logger.info("Livebox WAN IP: {}".format(livebox_wan_ip))
+        return livebox_wan_ip
     except exceptions.RequestException as e:
         raise Exception("Failed to query the livebox webservices: {}".format(e))
     except Exception as e:
@@ -104,34 +124,53 @@ def get_livebox_wan_ip():
 
 
 def parse_args():
-    global GANDI_API_TOKEN, GANDI_DOMAINS
+    global GANDI_API_TOKEN, GANDI_DOMAINS, GANDI_RECORD_TYPES, DRY_RUN
     parser = argparse.ArgumentParser(description='Update the DNS records for Gandi registered domains.')
-    parser.add_argument('--api-token', dest='api_token', action='store',
+    parser.add_argument('-t', '--api-token', dest='api_token', action='store',
                         help='The Gandi API token to use.')
-    parser.add_argument('--domains', dest='domains', action='store',
+    parser.add_argument('-d', '--domains', dest='domains', action='store',
                         help='A comma separated list of domains to update.')
-    parser.add_argument('--log', dest='log_level', action='store',
-                        help='The log level to display.', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+    parser.add_argument('-r', '--records', dest='records', action='store', choices=GANDI_RECORD_TYPES,
+                        help='The record type to update. Default is %(default)s.', default='all')
+    parser.add_argument('-l', '--log', dest='log_level', action='store',
+                        help='The log level to display. Default is %(default)s.',
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
                         default='INFO')
+    parser.add_argument('--dry-run', dest='dry_run', action='store_true',
+                        help='Do not push the updated domain records.')
+    parser.add_argument('--set-ip', dest='custom_ip', action='store',
+                        help='Update the domain records using the specified IP. '
+                             'Default is to extract the WAN address from the livebox on the LAN.')
     args = parser.parse_args()
     logger.setLevel(getattr(logging, args.log_level, None))
     GANDI_API_TOKEN = args.api_token if args.api_token else get_gandi_api_token()
     GANDI_DOMAINS = args.domains.split(',') if args.domains else get_gandi_domains()
+    logger.debug("Targeted domains: {}".format(GANDI_DOMAINS))
+    if not args.records == "all":
+        GANDI_RECORD_TYPES = list(args.records)
+    logger.debug("Targeted record types: {}".format(GANDI_RECORD_TYPES))
+    DRY_RUN = args.dry_run
+    logger.debug("Dry run mode: {}".format(DRY_RUN))
+    if args.custom_ip:
+        wan_ip = args.custom_ip
+        logger.debug("User defined WAN IP: {}".format(wan_ip))
+    else:
+        wan_ip = get_livebox_wan_ip()
+        logger.debug("Livebox WAN IP: {}".format(wan_ip))
+    return wan_ip
 
 
 def main():
     global GANDI_DOMAINS
     try:
-        parse_args()
-        livebox_ip = get_livebox_wan_ip()
-        logger.info("Livebox WAN IP: {}".format(livebox_ip))
+        wan_ip = parse_args()
         for domain in GANDI_DOMAINS:
             logger.info("Checking domain: {}".format(domain))
             records = get_domain_records(domain)
             gandi_ip = get_records_www_ip(records)
-            if not gandi_ip == livebox_ip:
+            if not gandi_ip == wan_ip:
                 logger.info("The IP address for domain {} must be updated ({}).".format(domain, gandi_ip))
-                updated_domain_records = update_gandi_domain_records(records, gandi_ip, livebox_ip)
+                updated_domain_records = update_gandi_domain_records(records, gandi_ip, wan_ip)
                 push_updated_domain_records(domain, updated_domain_records)
             else:
                 logger.info("The IP address configured for domain {} is valid: {}.".format(domain, gandi_ip))
